ee9f797dfa5cd7afe91a72100b220b03bcf3ed97
tangwr <firebolt2000@hotmail.com>
2017-01-17 16:25:35 +0800

    Testing cpp

---
+++ CS3201/CS3201/testing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int sum(int, int);
+
+void main(void) {
+	int x, y;
+
+	printf("Enter value x & y: ");
+	scanf_s("%d%d", &x, &y);
+
+	
+	std::cout << "Sum: " << x << " + " << y << " = " << sum(x,y)  << std::endl;
+	//return 0;
+}
+
+int sum(int x, int y) {
+	return x + y;
+}

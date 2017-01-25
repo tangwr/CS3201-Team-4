2aa466ba0a4e090e0c45786b22043e3a9945e6bc
swxsw <suwei9@gmail.com>
2017-01-26 00:43:21 +0800

    Added methods and variables into TNode
    Added methods and variables into TNode

--- SPA_Project/source/TNode.cpp
+++ SPA_Project/source/TNode.cpp
@@ -1,1 +1,13 @@
 #include "TNode.h"
+void TNode::setParent(TNode p) {
+    this->parent = &p;
+}
+void TNode::setChild(TNode c) {
+    this->child = &c;
+}
+TNode TNode::getChild() {
+    return *this->child;
+}
+TNode TNode::getParent() {
+    return *this->parent;
+}
--- SPA_Project/source/TNode.h
+++ SPA_Project/source/TNode.h
@@ -1,5 +1,12 @@
+#pragma once
 class TNode
 {
+private:
+    TNode* parent;
+    TNode* child;
 public:
-
+    void TNode::setParent(TNode p);
+    void TNode::setChild(TNode c);
+    TNode TNode::getChild();
+    TNode TNode::getParent();
 };

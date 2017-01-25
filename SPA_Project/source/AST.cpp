ef3d198c969a2f83263d1ed2ca8510d941269857
swxsw <suwei9@gmail.com>
2017-01-26 00:41:37 +0800

    Updated AST cpp and header
    Updated AST cpp and header with getter methods

--- SPA_Project/source/AST.cpp
+++ SPA_Project/source/AST.cpp
@@ -5,10 +5,25 @@ TNode AST::createTNode(nodeType) {
 }
 
 void AST::setAsRoot(TNode root) {
+    this->rootNode = root;
 }
 
 void AST::setParent(TNode p, TNode c) {
+    c.setParent(p);
 }
 
 void AST::setChild(TNode p, TNode c) {
+    p.setChild(c)
+}
+
+TNode AST::getRoot() {
+    return this->rootNode;
+}
+
+TNode AST::getParent(TNode c) {
+    return c.getParent();
+}
+
+TNode AST::getChild(TNode p) {
+    return c.getChild();
 }
--- SPA_Project/source/AST.h
+++ SPA_Project/source/AST.h
@@ -4,11 +4,18 @@
 
 class AST {
 
+private:
+
+    TNode rootNode;
+
 public:
 
     TNode createTNode(enum nodeType);
     void setAsRoot(TNode root);
     void setParent(TNode p, TNode c);
     void setChild(TNode p, TNode c);
+    TNode getRoot();
+    TNode getParent(TNode c);
+    TNode getChild(TNode p);
 
 };

a3fa729134c1bb5011e32d6d4c5842d67c2d6bc0
swxsw <suwei9@gmail.com>
2017-01-28 22:26:11 +0800

    Added comment to statement node definition
    Added comment to statement node definition

--- SPA_Project/source/TNodeStmt.cpp
+++ SPA_Project/source/TNodeStmt.cpp
@@ -3,7 +3,7 @@
 TNodeStmt::TNodeStmt() {
     this->parent = NULL;
     this->controlNode = NULL;
-    this->stmtNumber = -1;
+    this->stmtNumber = -1;//-1 means undefined
     this->cVar = false;
     this->children = *(new std::vector<TNode*>);
 }

2db9260915626fc4e26b98ef360467b54eb8020a
swxsw <suwei9@gmail.com>
2017-01-29 00:56:19 +0800

    Updated plus node definition
    Updated plus node definition

--- SPA_Project/source/TNodePlus.h
+++ SPA_Project/source/TNodePlus.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "TNodeExpr.h"
+
+class TNodePlus : public TNodeExpr {
+private:
+    TNode* parent;
+    std::vector<TNode*> children;
+    bool cVar;
+    std::string operatorValue;
+
+public:
+    TNodePlus();
+};

18e2c8d7d5c8afac975d7a04ab0ea55603c6c522
swxsw <suwei9@gmail.com>
2017-01-28 11:45:35 +0800

    Added nodeType enum
    Added nodeType enum

--- SPA_Project/source/TNode.h
+++ SPA_Project/source/TNode.h
@@ -1,10 +1,30 @@
 #pragma once
 #include <vector>
+
+enum nodeType {
+	assignNode,
+	callNode,
+	constNode,
+	exprNode,
+	ifNode,
+	minusNode,
+	plusNode,
+	procNode,
+	progNode,
+	refNode,
+	stmtNode,
+	stmtLstNode,
+	timesNode,
+	varNode,
+	whileNode
+};
+
 class TNode
 {
 private:
     TNode* parent;
     std::vector<TNode*> children;
+	//contol variable refers to if x, x is the control var
     TNode* controlNode;
     bool cVar;
 public:

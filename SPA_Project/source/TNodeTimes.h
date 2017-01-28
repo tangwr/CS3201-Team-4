0e50464aac9621cdb6eb2fc433c9e587cc9fc204
swxsw <suwei9@gmail.com>
2017-01-29 01:01:29 +0800

    Updated times node definition
    Updated times node definition

--- SPA_Project/source/TNodeTimes.h
+++ SPA_Project/source/TNodeTimes.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "TNodeExpr.h"
+
+class TNodeTimes : public TNodeExpr {
+private:
+    TNode* parent;
+    std::vector<TNode*> children;
+    bool cVar;
+    std::string operatorValue;
+
+public:
+    TNodeTimes();
+};

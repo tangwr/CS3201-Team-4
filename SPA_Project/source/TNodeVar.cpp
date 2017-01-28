f3900a599c25535181a4457095c8e89f3214f2e9
swxsw <suwei9@gmail.com>
2017-01-29 01:32:13 +0800

    Update variable node definition
    Update variable node definition

---
+++ SPA_Project/source/TNodeVar.cpp
@@ -0,0 +1,9 @@
+#include "TNodeVar.h"
+
+TNodeVar::TNodeVar() {
+    this->parent = NULL;
+    this->children = *(new std::vector<TNode*>);
+    this->cVar = false;
+    this->operatorValue = "none";
+    this->nodeValue = "node";
+}

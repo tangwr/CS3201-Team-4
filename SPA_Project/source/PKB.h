d951f3acf8cef20ec39bdd82f556da20a4d4aee7
swxsw <suwei9@gmail.com>
2017-01-31 22:49:42 +0800

    Updated PKB API
    Updated PKB API

--- SPA_Project/source/PKB.h
+++ SPA_Project/source/PKB.h
@@ -16,23 +16,48 @@ class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 class PKB {
 public:
     PKB();
-    TNode createNode(TNodeType nodeType);//returns created object
-    void setAsRoot(TNode root);
-    void setName(TNode node, string name);
-    void setValue(TNode node, int value);//stmt number or constant
+    //AST API
+    TNode* createNode(TNodeType nodeType);//returns created object
+    void setAsRoot(TNode* root);
+    void setName(TNode* node, string name);
+    //void setValue(TNode* node, int value);//stmt number or constant
+    bool setParent(TNode* child, TNode* parent);
+    bool setChild(TNode* parent, TNode* child);
+    
+    //while
+    bool setWhileVarNode(TNodeWhile whileNode, TNodeVar varNode);
+    bool setWhileStmtLstNode(TNodeWhile whileNode, TNodeStmtLst stmtLstNode);
+    //if
+    //call
+    //assign
+    bool setAssignVarNode(TNodeAssign assignNode, TNodeVar varNode);
+    bool setAssignExpString(TNodeAssign assignNode, string expressionString);
+    bool setAssignExpNode(TNodeAssign assignNode, TNodeExpr expNode);
+    //var
+    bool setVarNodeValue(TNodeVar varNode, int value);
+    //const
+    bool setConstNodeValue(TNodeConst constNode, int value);
+    //stmtList
+    bool addStmtLstStmtNode(TNodeStmtLst stmtListNode, TNodeStmt stmtNode);
+    //proc
+    bool setProcStmtLstNode(TNodeProc procNode, TNodeStmtLst stmtListNode);
+    //prog
+    bool addProgProcNode(TNodeProg progNode, TNodeProc procNode);
+    //
 
+    //TABLE
 	static VarTable* varTable; 
 	static int setProcToAST(PROC p, TNode* r);
 	static TNode* getRootAST (PROC p);
 	
-	void addProcedure(String name);
-	void addVariable(String name);
-	void addStatement(int statementNum, TnodeType type);
+	void addProcedure(string name);
+	void addVariable(string name);
+	void addStatement(int statementNum, TNodeType type);
 
 	void addFollows(int statementNum, list<int> followList);
 	void addParents(int statementNum, list<int> parentList);
-	void addUses(int statementNum, String variable);
-	void addModifies(int statementNum, String Variable);
+	void addUses(int statementNum, string variable);
+	void addModifies(int statementNum, string Variable);
 
 private:
     AST ast;
--- SPA_Project/source/TNodeType.h
+++ SPA_Project/source/TNodeType.h
@@ -1,5 +1,5 @@
 #pragma once
 enum TNodeType {
 	progNode, procNode, assignNode, whileNode, ifNode, callNode,
-	plusNode, minusNode, timesNode, varNode, constNode
+	plusNode, minusNode, timesNode, varNode, constNode, lstNode, 
 };

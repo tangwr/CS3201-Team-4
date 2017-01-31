26b4540286a9090f8d19f6774946b35355626b1d
swxsw <suwei9@gmail.com>
2017-01-31 10:08:53 +0800

    Added AST api into pkb
    Added AST api into pkb

--- SPA_Project/source/PKB.cpp
+++ SPA_Project/source/PKB.cpp
@@ -10,10 +10,27 @@ using namespace std;
 #include "PKB.h"
 #include "TNode.h"
 
+PKB::PKB() {
+}
+
 int PKB::setProcToAST(PROC p, TNode* r) {
 	return NULL;
 }
 
 TNode* PKB::getRootAST (PROC p){
 	return NULL;
+}
+
+TNode PKB::createNode(TNodeType nodeType) {//return TNode object
+    TNode* newNode = ast.createTNode(nodeType);
+    return *newNode;
+}
+void PKB::setAsRoot(TNode root) {
+    ast.setAsRoot(root);
+}
+void PKB::setName(TNode node, string name) {
+
+}
+void PKB::setValue(TNode node, int value) {//stmt number or constant
+
 }
--- SPA_Project/source/PKB.h
+++ SPA_Project/source/PKB.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "AST.h"
 
 using namespace std;
 typedef short PROC;
@@ -14,8 +15,16 @@ class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
+    PKB();
+    TNode createNode(TNodeType nodeType);//returns created object
+    void setAsRoot(TNode root);
+    void setName(TNode node, string name);
+    void setValue(TNode node, int value);//stmt number or constant
+
 	static VarTable* varTable; 
 	static int setProcToAST(PROC p, TNode* r);
 	static TNode* getRootAST (PROC p);
 
+private:
+    AST ast;
 };

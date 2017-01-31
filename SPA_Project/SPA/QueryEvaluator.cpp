702f713c6c45967c4b36875b3c00a91ff29a9bbc
tangwr <firebolt2000@hotmail.com>
2017-02-01 02:08:39 +0800

    query evaluator

--- SPA_Project/SPA/QueryEvaluator.cpp
+++ SPA_Project/SPA/QueryEvaluator.cpp
@@ -3,40 +3,82 @@
 //Public method
 
 QueryEvaluator::QueryEvaluator(){
-
 }
 
 std::list<std::string> QueryEvaluator::evaluate(QueryTree qt) {
 	std::list<std::string> results;
-
+	results = evaluateQuery(qt);
 	return results
 }
 
 
 //Private method
 std::list<std::string> QueryEvaluator::evaluateQuery(QueryTree qt) {
-	<PKB_class> pkb;
+	/*
+		IDENT: LETTER(LETTER | DIGIT | '#')*
+		synonym: IDENT
+		stmtRef: synonym | '_' | INTEGER
+		entRef: synonym | '_' | "IDENT"
+		Modifies/Uses ('stmtRef', 'entRef')
+		Parent/Follow ('stmtRef', 'stmtRef')
+	*/
 
-	<datatype> varTable = pkb.getVarTable();
-	<datatype> stmtTable = pkb.getStmtTable();
-	<datatype> assignTable = pkb.getAssignTable();
-	<datatype> parentTable = pkb.getParentTable();
-	<datatype> modifyTable = pkb.getModifyTable();
-	<datatype> followTable = pkb.getFollowTable();
+
+	/*
+		Get the individual parts of the query string
+		Example:
+			assignment a, a1; variable v;
+			- Select a such that Uses(a,v) pattern a1(v,_)
+
+			assign a, statement s, variable v;
+			- Select a pattern a("x", _"y"_)
+			- Select s such that Follows(1,s)
+			- Select s such that Follows(s,2)
+			- Select s usch that Follows*(1,s)
+			- Select s such that Follows*(s,2)
+			- Select s such that Parent(2,s)
+			- Select s such that Parent(s,3)
+			- Select s such that Parent*(2,s)
+			- Select s such that Parent*(s,3)
+			- Select v such that Modifies(1,v)
+			- Select a such that Modifies(a,"x")
+			- Select BOOLEAN such that Modifies(3,"x")
+
+	*/
+	//Declaration
+	std::vector<std::string> stmt = qt.stmt;
+	std::vector<std::string> assign = qt.assign;
+	std::vector<std::string> whileS = qt.whileS;
+	std::vector<std::string> variable = qt.variable;
+	std::vector<std::string> constant = qt.constant;
+	std::vector<std::string> prog_line = qt.prog_line;
+	
+	//Select synonym the value to be return
+	std::vector<std::string> synonym = qt.synonym; //a.k.a 'syntax'
+
+	//The clause
+	
+	/*
+		'such that' clause
+		relRef: Modifies, Uses, Parent, Parent*, Follows, Follows*
+	*/
+	std::string relRef = qt.relRef;
+	std::string relRef_value = qt.relRef_value;
+	std::string relRef_left_value = getLeftValue(relRef_value);
+	std::string relRef_right_value = getRightValue(relRef_value);
+
+	/*
+		'pattern' clause 
+		syn-assign (entRef, expression-spec)
+		syn-assign must be declared as stnonym of assignment (degin entity 'assign')
+	*/
+	std::string syn_assign = qt.syn_assign;
+	std::string syn_assign_value = qt.syn_assign_value;
+	std::string syn_assign_left_value = getLeftValue(syn_assign_value);
+	std::string syn_assign_right_value = getRightValue(syn_assign_value);
 
 
 
-	std::string syntax = qt.syntax;
-	std::string assign = qt.assign;
-	std::string variable = qt.variable;
-	std::string statement = qt.statement;
-
-	validateVariable = validateQuery(varTable, variable);
-	validateStmt = validateQuery(stmtTable, statement);
-	validateAssign = validateQuery(assignTable, assign);
-	validateParent = validateQuery(parentTable, parent);
-	validateModify = validateQuery(modifyTable, modify);
-	validateFollow = validateQuery(followTable, follow);
 	
 }
 
@@ -45,4 +87,48 @@ bool QueryEvaluator::validateQuery(<datatype> table, std::string token) {
 	bool result;
 
 	return result;
+}
+
+
+
+
+std::string QueryEvaluator::getLeftValue(std::string input)
+{
+	int delim_pos = getDelimPos(input, ',');
+	std::string value = input.substr(1, delim_pos - 1);
+	
+	return removeSpaces(value);
+}
+
+std::string QueryEvaluator::getRightValue(std::string input)
+{
+	int delim_pos = getDelimPos(input, ',') + 1;
+	std::string value = input.substr(delim_pos, input.length() - delim_pos - 1);
+	
+	return removeSpaces(value);
+}
+
+int QueryEvaluator::getDelimPos(std::string input, char delim) {
+	int delim_pos = 0;
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] == delim) {
+			delim_pos = i;
+		}
+	}
+
+	return delim_pos;
+}
+
+std::string QueryEvaluator::removeSpaces(std::string input)
+{
+	int length = input.length();
+	for (int i = 0; i < length; i++) {
+		if (input[i] == ' ')
+		{
+			input.erase(i, 1);
+			length--;
+			i--;
+		}
+	}
+	return input;
 }
--- SPA_Project/SPA/QueryEvaluator.h
+++ SPA_Project/SPA/QueryEvaluator.h
@@ -4,9 +4,18 @@
 #include <vector>
 #include <list>
 #include "QueryTree.h"
+#include "ModifiesTable.h"
+#include "UsesTable.h"
+#include "ParentsTable.h"
+#include "FollowsTable.h"
+
 
 class QueryEvaluator {
 private:
+	std::string getLeftValue(std::string);
+	std::string getRightValue(std::string);
+	std::string removeSpaces(std::string);
+	int getDelimPos(std::string, char);
 	std::string dummy = "unknown";
 	std::list<std::string> evaluateQuery(QueryTree);
 	bool validateQuery(<datatype_table>, std::string);

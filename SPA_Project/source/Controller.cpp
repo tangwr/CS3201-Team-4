7e41be6b2d132b852af1113db2b0e6473d3e4c5e
Tan Qin Hui <destqh@hotmail.com>
2017-02-05 21:20:53 +0800

    insert PQL files to sln

--- SPA_Project/source/Controller.cpp
+++ SPA_Project/source/Controller.cpp
@@ -1,8 +1,7 @@
 #include "Controller.h"
 #include "Parser.h"
 #include "DesignExtractor.h"
-#include "Pattern.h"
-#include "Type.h"
+
 
 Controller::Controller() {
 	pkb = new PKB();
@@ -25,25 +24,6 @@ void Controller::processSource(string source) {
 }
 
 string Controller::processQuery(string source) {
-	Pattern pat("a", Type::ASSIGN, "_", Type::ANYTHING, true, "x+i", Type::STRINGVARIABLE);
-	pat.setUnderScore(true);
-
-	/*
-	vector<int> result = pat.getResultRelwithLeft(pkb);
-
-	cout << endl << "result of pattern: ";
-	for (int i = 0; i < (int)result.size(); i++) {
-		cout << result[i] << " ";
-	}
-	cout << endl;
-	*/
-
-	if (pat.hasRel(pkb)) {
-		cout << "Have Relation" << endl;
-	}
-	else {
-		cout << "No Relation" << endl;
-	}
 
 	return "";
 }

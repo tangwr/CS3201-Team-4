35d3fd488ef9af3421932ba6b220c4abed94787d
A0112659 <A0112659@u.nus.edu>
2017-02-05 13:46:05 +0800

    Update Pattern.h

--- SPA_Project/source/Pattern.h
+++ SPA_Project/source/Pattern.h
@@ -19,25 +19,24 @@ private:
 	PKB *pkb;
 	bool isRel;
 	bool isUnderScore;
-	string syn;
-	Type synType;
+	string factor;
+	Type factorType;
 
 
 public:
 
-	Pattern(string lc, Type lcType, string rc, Type rcType, bool underscore);
+	Pattern(string lc, Type lcType, string rc, Type rcType, bool underscore, string factor, Type factorType);
 	bool hasRel(PKB *pkb);
 	void setUnderScore(bool);
-	void setSynonym(string);
-	string getSynonym();
-	Type getSynType();
+	void setFactor(string);
 	bool getUnderScore();
 	vector<int> getWithRelToLeft(PKB *pkb);
 	vector<int> getWithRelToRight(PKB *pkb);
 	string getLeftChild();
 	string getRightChild();
+	string getFactor();
 	Type getLeftChildType();
 	Type getRightChildType();
-
+	Type getFactorType();
 };
 #endif

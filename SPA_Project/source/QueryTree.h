69aafd34a38755b7d920b0dc96df5fa5390e730b
A0112659 <A0112659@u.nus.edu>
2017-02-17 19:42:19 +0800

    Update QueryTree.h for Iteration 2

--- SPA_Project/source/QueryTree.h
+++ SPA_Project/source/QueryTree.h
@@ -9,32 +9,38 @@
 
 #include "Type.h"
 #include "Clause.h"
+#include "Parameter.h"
 
 using namespace std;
 
 class QueryTree {
 private:
 
-	unordered_map<string, Type> select;
-	unordered_map<string, Type> comonVar;
-	bool isCommonVar;
-	vector<Clause*> limits;
-	vector<Clause*> unlimits;
+	//unordered_map<string, Type> select;
+	//unordered_map<string, Type> comonVar;
+	//bool isCommonVar;
+	//vector<Clause*> limits;
+	//vector<Clause*> unlimits;
+	vector<Parameter> select;
+	vector<Clause*> resultList;
 
 public:
 	QueryTree();
-    void insertSelect(string, Type);
-	void insertComonVar(string, Type);
-	void insertLimits(Clause*);
-	void insertUnLimits(Clause*);
-	void setIsComonVar(bool);
-	void clearComonMap();
+   // void insertSelect(string, Type);
+	//void insertComonVar(string, Type);
+	//void insertLimits(Clause*);
+	//void insertUnLimits(Clause*);
+	//void setIsComonVar(bool);
+	void insertSelect(Parameter p);
+	void insertResult(Clause*);
 	
-	vector<Clause*> getLimits();
-	vector<Clause*> getUnLimits();
-	unordered_map<string, Type> getSelect();
-	unordered_map<string, Type> getComonVar();
-	bool getIsComonVar();
+	//vector<Clause*> getLimits();
+	//vector<Clause*> getUnLimits();
+	//unordered_map<string, Type> getSelect();
+	//unordered_map<string, Type> getComonVar();
+	//bool getIsComonVar();
+	vector<Clause*> getResult();
+	vector<Parameter> getSelect();
 
 };
 #endif

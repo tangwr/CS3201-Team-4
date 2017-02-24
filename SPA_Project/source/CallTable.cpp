5c9581dbbd6493acffc7fafff4d8af9e14c2f6f7
swxsw <suwei9@gmail.com>
2017-02-25 01:49:19 +0800

    Updated call table to include procedure to procedure
    Updated call table to include procedure to procedure calls

--- SPA_Project/source/CallTable.cpp
+++ SPA_Project/source/CallTable.cpp
@@ -8,24 +8,52 @@ CallTable::CallTable() {
 }
 
 bool CallTable::setStmtCallProc(int stmtId, int procId) {
-	if (this->callList.find(stmtId) != this->callList.end()) {
+	if (this->stmtCallProcList.find(stmtId) != this->stmtCallProcList.end()) {
 		return false;
 	}
 	else {
-		this->callList.insert({stmtId, procId});
+		this->stmtCallProcList.insert({stmtId, procId});
 		this->size++;
 		return true;
 	}
 }
 
+bool CallTable::setProcCallProc(int callerProcId, int calledProcId) {
+
+    unordered_map<int, unordered_set<int>>::iterator it;
+    it = procCallProcList.find(callerProcId);
+    unordered_set<int> procSet;//procSet should be empty
+    if (it != procCallProcList.end()) {
+        procSet = it->second;
+        if (procSet.find(calledProcId) != procSet.end()) {
+            return false;
+        }
+        procCallProcList.erase(it);
+    }
+    procSet.insert(calledProcId);
+    procCallProcList.insert(make_pair(callerProcId, procSet));
+    return true;
+}
+
 int CallTable::getCallProc(int stmtId) {
-	auto it = this->callList.find(stmtId);
-	if (it != callList.end()) {
+	auto it = this->stmtCallProcList.find(stmtId);
+	if (it != stmtCallProcList.end()) {
 		return it->second;
 	}
 	else {
 		return -1;
 	}
+}
+
+unordered_set<int> CallTable::getProcCallProc(int callerProcId) {
+    auto it = this->procCallProcList.find(callerProcId);
+    if (it != procCallProcList.end()) {
+        return it->second;
+    }
+    else {
+        unordered_set<int> emptySet;
+        return emptySet;
+    }
 }
 
 bool CallTable::isStmtInTable(int stmtId)
@@ -41,7 +69,7 @@ void CallTable::printContents() {
 	cout << "Call Table" << endl;
 	cout << "====================" << endl;
 
-	for (auto& it : this->callList)
+	for (auto& it : this->stmtCallProcList)
 		cout << it.first << ": " << it.second << endl;
 
 	cout << "====================" << endl;
--- SPA_Project/source/CallTable.h
+++ SPA_Project/source/CallTable.h
@@ -5,19 +5,23 @@
 #include <string>
 #include <utility>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class CallTable {
 private:
-	unordered_map<int, int> callList;
-	int size;
+	unordered_map<int, int> stmtCallProcList;//stmtId, procId
+    unordered_map<int, unordered_set<int>> procCallProcList;//procId, procId
+	int size;//check if need another size
 
 public:
 	CallTable();
 	int getSize();
 	void printContents();
 	bool setStmtCallProc(int stmtId, int procId);
+    bool setProcCallProc(int callerProcId, int calledProcId);
 	int getCallProc(int stmtId);
+    unordered_set<int> getProcCallProc(int callerProcId);
 	bool isStmtInTable(int stmtId);
 };

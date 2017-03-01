94fb440ec2a3f901596fca776aac98da67c12975
A0112659 <A0112659@u.nus.edu>
2017-03-01 16:34:30 +0800

    Update Uses.cpp

--- SPA_Project/source/Uses.cpp
+++ SPA_Project/source/Uses.cpp
@@ -13,6 +13,15 @@
 Uses::Uses(Parameter lc, Parameter rc) {
     leftChild = lc;
 	rightChild = rc;
+
+	if (lc.getParaType() != Type::CONSTANT && lc.getParaType() != Type::BOOLEAN && lc.getParaType() != Type::ANYTHING
+		&& lc.getParaType() != Type::STRINGVARIABLE && lc.getParaType() != Type::INTEGER) {
+		synList.push_back(lc);
+	}
+	if (rc.getParaType() != Type::CONSTANT && rc.getParaType() != Type::BOOLEAN && rc.getParaType() != Type::ANYTHING
+		&& rc.getParaType() != Type::STRINGVARIABLE && rc.getParaType() != Type::INTEGER) {
+		synList.push_back(rc);
+	}
 }
 bool Uses::hasRel(PKB *pkbSource) {
 	vector<int> result;
@@ -191,4 +200,7 @@ Parameter Uses::getLeftChild() {
 }
 Parameter Uses::getRightChild() {
 	return rightChild;
+}
+vector<Parameter> Uses::getSynList() {
+	return synList;
 }

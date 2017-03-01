7866efbf6daef7c95d0f721e5f6befbb45e2d056
bthjehiel <jehielmisc@gmail.com>
2017-03-01 16:28:38 +0800

    Update ParentStar.cpp

--- SPA_Project/source/ParentStar.cpp
+++ SPA_Project/source/ParentStar.cpp
@@ -2,6 +2,180 @@
 #include "ParentStar.h"
 #include "Clause.h"
 
+
+ParentStar::ParentStar(Parameter lc, Parameter rc) {
+	leftChild = lc;
+	rightChild = rc;
+}
+
+
+
+ResultTable* ParentStar::execute(PKB* pkb) {
+	if (isNumber(leftChild)) {
+		if (isNumber(rightChild)) {
+			return getParentStarNumNum(pkb);
+		}
+		else if (isSynonym(rightChild)) {
+			return getParentStarNumSyn(pkb);
+		}
+	}
+	else if (isSynonym(leftChild)) {
+		if (isNumber(rightChild)) {
+			return getParentStarSynNum(pkb);
+		}
+		else if (isSynonym(rightChild)) {
+			return getParentStarSynSyn(pkb);
+		}
+	}
+	return result;
+}
+
+ResultTable* ParentStar::getParentStarNumNum(PKB* pkb) {
+	int leftArgument = stoi(leftChild.getParaName());
+	int rightArgument = stoi(rightChild.getParaName());
+	if (!isValidStmtNo(leftArgument, pkb)) {
+		result->setBoolean(false);
+		return result;
+	}
+	if (!isValidStmtNo(rightArgument, pkb)) {
+		result->setBoolean(false);
+		return result;
+	}
+
+	vector<int> Parents = pkb->getStmtParentStarStmt(rightArgument);
+	for (int i = 0; i < Parents.size(); i++) {
+		if (Parents[i] == leftArgument) {
+			result->setBoolean(true);
+			break;
+		}
+	}
+	return false;
+}
+
+ResultTable* ParentStar::getParentStarNumSyn(PKB* pkb) {
+	int leftArgument = stoi(leftChild.getParaName());
+	if (!isValidStmtNo(leftArgument, pkb)) {
+		return result;
+	}
+	else { //if left is a valid statement number, Parents(num, syn)
+		result->setSynList(vector<Parameter>({ rightChild }));
+		return getAllChildrenStar(vector<int>(leftArgument), pkb);
+		//result = filterType(tempResult, rightChildType, pkb);
+		//return result;
+	}
+}
+
+ResultTable* ParentStar::getParentStarSynNum(PKB* pkb) {
+	int rightArgument = stoi(rightChild.getParaName());
+	if (!isValidStmtNo(rightArgument, pkb)) {
+		return result;
+	}
+	else { //if right is a valid statement number, Parents(syn, num)
+		result->setSynList(vector<Parameter>({ leftChild }));
+		return getAllParentStar(vector<int>(rightArgument), pkb);
+	}
+}
+
+ResultTable* ParentStar::getParentStarSynSyn(PKB* pkb) {
+	if (leftChild.getParaName().compare(rightChild.getParaName()) == 0) {
+		return result;
+	}
+
+
+	result->setSynList(vector<Parameter>({ leftChild, rightChild }));
+	vector<int> right = getTypeStmt(rightChild.getParaType(), pkb);
+	return getAllParentStar(right, pkb);
+	//return result;
+}
+
+vector<int> ParentStar::getTypeStmt(Type type, PKB* pkb) {
+	switch (type) {
+	case PROG_LINE:
+	case STMT:
+	case ANYTHING: {
+		int numOfStmt = pkb->getNumOfStmt();
+		vector<int> stmtList(numOfStmt);
+		for (int i = 0; i < numOfStmt; i++) {
+			stmtList[i] = i + 1;
+		}
+		return stmtList;
+	}
+	case WHILE:
+		return pkb->getAllWhileStmt();
+	case ASSIGN:
+		return pkb->getAllAssignStmt();
+	}
+	return vector<int>();
+}
+
+
+ResultTable* ParentStar::getAllParentStar(vector<int> list, PKB* pkb) {
+	vector<int> parentStar;
+	for (int i = 0; i < list.size(); i++) {
+		parentStar = pkb->getStmtParentStarStmt(list[i]);
+		for (int j = 0; j < parentStar.size(); j++) {
+			if (isStmtType(parentStar[j], leftChild, pkb)) {
+				result->insertTuple(vector<int>(parentStar[j], list[i]));
+			}
+		}
+
+	}
+	return result;
+}
+
+ResultTable* ParentStar::getAllChildrenStar(vector<int> list, PKB* pkb) {
+	vector<int> childrenStar;
+	for (int i = 0; i < list.size(); i++) {
+		childrenStar = pkb->getStmtChildrenStarStmt(list[i]);
+		for (int j = 0; j < childrenStar.size(); j++) {
+			if (isStmtType(childrenStar[j], rightChild, pkb)) {
+				result->insertTuple(vector<int>(list[i], childrenStar[j]));
+			}
+		}
+	}
+	return result;
+}
+
+bool ParentStar::isStmtType(int stmtId, Parameter parameter, PKB* pkb) {
+	Type type = parameter.getParaType();
+	if (stmtId < 1)
+		return false;
+	switch (type) {
+	case WHILE:
+		return pkb->isStmtInWhileTable(stmtId);
+	case ASSIGN:
+		return pkb->isStmtInAssignTable(stmtId);
+	case PROG_LINE:
+	case STMT:
+	case ANYTHING:
+		return true;
+	}
+	return false;
+}
+
+bool ParentStar::isNumber(Parameter parameter) {
+	Type type = parameter.getParaType();
+	return (type == INTEGER);
+}
+
+bool ParentStar::isSynonym(Parameter parameter) {
+	Type type = parameter.getParaType();
+	return (type == ASSIGN || type == WHILE || type == STMT || type == ANYTHING || type == PROG_LINE);
+}
+
+bool ParentStar::isValidStmtNo(int stmtId, PKB* pkb) {
+	return ((stmtId > 0) && (stmtId <= pkb->getNumOfStmt()));
+}
+
+Parameter ParentStar::getLeftChild() {
+	return leftChild;
+}
+Parameter ParentStar::getRightChild() {
+	return rightChild;
+}
+
+
+/*
 ParentStar::ParentStar(string lc, Type lcType, string rc, Type rcType) {
 	leftChild = lc;
 	rightChild = rc;
@@ -254,3 +428,4 @@ Type ParentStar::getRightChildType() {
 ClauseType ParentStar::getClauseType() {
 	return PARENTSTAR;
 }
+*/

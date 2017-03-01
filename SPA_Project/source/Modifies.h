72f3cbc06602979af72010c3aa0435732fedf6ca
A0112659 <A0112659@u.nus.edu>
2017-03-01 12:20:15 +0800

    Update Modifies.h

--- SPA_Project/source/Modifies.h
+++ SPA_Project/source/Modifies.h
@@ -15,25 +15,32 @@ using namespace std;
 class Modifies : public Clause {
 private:
 	//ClauseType cltype;
-	string leftChild;
-	string rightChild;
-	Type leftChildType;
-	Type rightChildType;
+	//string leftChild;
+	//string rightChild;
+	//Type leftChildType;
+	//Type rightChildType;
+	Parameter leftChild;
+	Parameter rightChild;
 	PKB *pkb;
 	bool isRel;
 	
 
 public:
 
-	Modifies(string,Type, string, Type);
+	//Modifies(string,Type, string, Type);
+	Modifies(Parameter lc, Parameter rc);
 	bool hasRel(PKB *pkb);
 	vector<int> getWithRelToLeft(PKB *pkb);
 	vector<int> getWithRelToRight(PKB *pkb);
+	/*
 	string getLeftChild();
 	string getRightChild();
 	Type getLeftChildType();
 	Type getRightChildType();
-	ClauseType getClauseType();
+	*/
+
+	Parameter getLeftChild();
+	Parameter getRightChild();
 
 };
 #endif

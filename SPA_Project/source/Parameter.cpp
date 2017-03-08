0fdc8db789072aecc786d115bf7a9929ab583e6b
A0112659 <A0112659@u.nus.edu>
2017-03-08 16:29:13 +0800

    Update Parameter.cpp

--- SPA_Project/source/Parameter.cpp
+++ SPA_Project/source/Parameter.cpp
@@ -9,6 +9,14 @@ Parameter::Parameter() {
 Parameter::Parameter(string name, Type type) {
 	paraName = name;
 	paraType = type;
+
+	if (paraType != CONSTANT && paraType != BOOLEAN && paraType != ANYTHING
+		&& paraType != STRINGVARIABLE && paraType != INTEGER) {
+		isSyn = true;
+	}
+	else {
+		isSyn = false;
+	}
 }
 
 string Parameter::getParaName() {
@@ -25,4 +33,18 @@ void Parameter::setParaName(string name) {
 
 void Parameter::setParaType(Type type) {
 	paraType = type;
+}
+
+bool Parameter::isSynonym() {
+	return isSyn;
+}
+
+bool Parameter::isSame(Parameter paraObj) {
+	if (paraName.compare(paraObj.getParaName()) != 0) {
+		return false;
+	}
+	if (paraType != paraObj.getParaType) {
+		return false;
+	}
+	return true;
 }

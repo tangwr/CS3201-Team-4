cc037975903f4c2bd98e513d438145c8a65989c5
bthjehiel <jehielmisc@gmail.com>
2017-03-12 02:52:25 +0800

    Update ParentStar.h

--- SPA_Project/source/ParentStar.h
+++ SPA_Project/source/ParentStar.h
@@ -15,18 +15,21 @@ private:
 
 	Parameter leftChild;
 	Parameter rightChild;
-	ResultTable* result;
+	ResultTable result;
 	bool isNumber(Parameter);
 	bool isSynonym(Parameter);
 	bool isValidStmtNo(int, PKB*);
+	bool isLeftChild(Parameter);
+	bool isParentStar(PKB*, int, int);
 	bool isStmtType(int, Parameter, PKB*);
-	ResultTable* getAllParentStar(vector<int>, PKB*);
-	ResultTable* getAllChildrenStar(vector<int>, PKB*);
-	vector<int> getTypeStmt(Type, PKB*);
-	ResultTable* getParentStarNumNum(PKB*);
-	ResultTable* getParentStarNumSyn(PKB*);
-	ResultTable* getParentStarSynNum(PKB*);
-	ResultTable* getParentStarSynSyn(PKB*);
+	ResultTable getAllParentStar(vector<int>, PKB*);
+	ResultTable getAllChildrenStar(vector<int>, PKB*);
+	unordered_set<int> getTypeStmt(Type, PKB*);
+	ResultTable getParentStarNumNum(PKB*, int, int);
+	ResultTable getParentStarNumSyn(PKB*, unordered_set<int>, int);
+	ResultTable getParentStarSynNum(PKB*, unordered_set<int>, int);
+	ResultTable getParentStarSynSyn(PKB*, unordered_set<int>, unordered_set<int>);
+	ResultTable getParentStarSynSyn(PKB*, ResultTable*);
 	vector<Parameter> synList;
 
 	/*
@@ -51,7 +54,9 @@ public:
 	ParentStar(Parameter lc, Parameter rc);
 	Parameter getLeftChild();
 	Parameter getRightChild();
-	ResultTable* execute(PKB*);
+	ResultTable evaluate(PKB*);
+	ResultTable evaluate(PKB*, ResultTable*);
+	void insertSynList(Parameter);
 	vector<Parameter> getSynList();
 	/*
 	ParentStar(string, Type, string, Type);

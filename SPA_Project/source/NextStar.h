012fc90fb7f99c920c9fd24f4f3ad28d3bae1b21
bthjehiel <jehielmisc@gmail.com>
2017-03-12 02:56:04 +0800

    Update NextStar.h

--- SPA_Project/source/NextStar.h
+++ SPA_Project/source/NextStar.h
@@ -5,6 +5,7 @@
 #include "Clause.h"
 #include "Type.h"
 #include "PKB.h"
+#include "unordered_set"
 #include "ResultTable.h"
 
 using namespace std;
@@ -14,10 +15,27 @@ private:
 	Parameter leftChild;
 	Parameter rightChild;
 	vector<Parameter> synList;
+	ResultTable result;
+	bool isNumber(Parameter);
+	bool isSynonym(Parameter);
+	bool isValidStmtNo(int, PKB*);
+	bool isStmtType(int, Parameter, PKB*);
+	void getAllNextStar(int, unordered_set<int>, PKB*);
+	void getAllPrevStar(int, unordered_set<int>, PKB*);
+	vector<int> getTypeStmt(Type, PKB*);
+	ResultTable getNextStarNumNum(PKB*);
+	ResultTable getNextStarNumSyn(PKB*);
+	ResultTable getNextStarSynNum(PKB*);
+	ResultTable getNextStarSynSyn(PKB*);
 
 
 public:
 	NextStar(Parameter lc, Parameter rc);
+	Parameter getLeftChild();
+	Parameter getRightChild();
+	ResultTable execute(PKB*);
+	void insertSynList(Parameter);
+	vector<Parameter> getSynList();
 
 };
 #endif

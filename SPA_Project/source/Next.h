a3ade20b87935701e0f657df40ac6f0bc3caf559
bthjehiel <jehielmisc@gmail.com>
2017-03-12 02:53:19 +0800

    Update Next.h

--- SPA_Project/source/Next.h
+++ SPA_Project/source/Next.h
@@ -5,6 +5,7 @@
 #include "Clause.h"
 #include "Type.h"
 #include "PKB.h"
+#include "unordered_set"
 #include "ResultTable.h"
 
 using namespace std;
@@ -13,11 +14,31 @@ class Next : public Clause {
 private:
 	Parameter leftChild;
 	Parameter rightChild;
+	ResultTable result;
 	vector<Parameter> synList;
+	bool isNumber(Parameter);
+	bool isSynonym(Parameter);
+	bool isLeftChild(Parameter);
+	bool isRightChild(Parameter);
+	bool isValidStmtNo(int, PKB*);
+	bool isStmtType(int, Parameter, PKB*);
+	vector<int> getTypeStmt(Type, PKB*);
+	ResultTable getNextNumNum(PKB*);
+	ResultTable getNextNumSyn(PKB*);
+	ResultTable getNextNumSyn(PKB*, vector<vector<int>>);
+	ResultTable getNextSynNum(PKB*);
+	ResultTable getNextSynNum(PKB*, vector<vector<int>>);
+	ResultTable getNextSynSyn(PKB*);
+	ResultTable getNextSynSyn(PKB*, vector<vector<int>>);
 
 
 public:
 	Next(Parameter lc, Parameter rc);
+	ResultTable execute(PKB*);
+	Parameter getLeftChild();
+	Parameter getRightChild();
+	void insertSynList(Parameter);
+	vector<Parameter> getSynList();
 
 };
 #endif

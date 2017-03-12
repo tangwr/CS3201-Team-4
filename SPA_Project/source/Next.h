937b0d14aec52c299d695f10173203b31483ec75
bthjehiel <jehielmisc@gmail.com>
2017-03-12 14:17:17 +0800

    Update Next.h

--- SPA_Project/source/Next.h
+++ SPA_Project/source/Next.h
@@ -18,23 +18,20 @@ private:
 	vector<Parameter> synList;
 	bool isNumber(Parameter);
 	bool isSynonym(Parameter);
-	bool isLeftChild(Parameter);
-	bool isRightChild(Parameter);
+	bool isNext(PKB*, int, int);
 	bool isValidStmtNo(int, PKB*);
-	bool isStmtType(int, Parameter, PKB*);
-	vector<int> getTypeStmt(Type, PKB*);
-	ResultTable getNextNumNum(PKB*);
-	ResultTable getNextNumSyn(PKB*);
-	ResultTable getNextNumSyn(PKB*, vector<vector<int>>);
-	ResultTable getNextSynNum(PKB*);
-	ResultTable getNextSynNum(PKB*, vector<vector<int>>);
-	ResultTable getNextSynSyn(PKB*);
-	ResultTable getNextSynSyn(PKB*, vector<vector<int>>);
+	unordered_set<int> getTypeStmt(Type, PKB*);
+	ResultTable getNextNumNum(PKB*, int, int);
+	ResultTable getNextNumSyn(PKB*, unordered_set<int>, int);
+	ResultTable getNextSynNum(PKB*, unordered_set<int>, int);
+	ResultTable getNextSynSyn(PKB*, unordered_set<int>, unordered_set<int>);
+	ResultTable getNextSynSyn(PKB*, ResultTable*);
 
 
 public:
 	Next(Parameter lc, Parameter rc);
-	ResultTable execute(PKB*);
+	ResultTable evaluate(PKB*);
+	ResultTable evaluate(PKB*, ResultTable*);
 	Parameter getLeftChild();
 	Parameter getRightChild();
 	void insertSynList(Parameter);

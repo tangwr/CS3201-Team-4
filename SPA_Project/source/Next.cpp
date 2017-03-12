5761371a88dc4f2023d6b0ee93b7174b929a6ea5
bthjehiel <jehielmisc@gmail.com>
2017-03-12 14:19:11 +0800

    Update Next.cpp

--- SPA_Project/source/Next.cpp
+++ SPA_Project/source/Next.cpp
@@ -10,6 +10,206 @@ Next::Next(Parameter lc, Parameter rc) {
 	rightChild = rc;
 }
 
+
+ResultTable Next::evaluate(PKB* pkb) {
+	if (isNumber(leftChild)) {
+		if (isNumber(rightChild)) {
+			return getNextNumNum(pkb);
+		}
+		else if (isSynonym(rightChild)) {
+			return getNextNumSyn(pkb, getTypeStmt(rightChild.getParaType(), pkb), stoi(leftChild.getParaName()));
+		}
+	}
+	else if (isSynonym(leftChild)) {
+		if (isNumber(rightChild)) {
+			return getNextSynNum(pkb, getTypeStmt(leftChild.getParaType(), pkb), stoi(rightChild.getParaName()));
+		}
+		else if (isSynonym(rightChild)) {
+			return getNextSynSyn(pkb, getTypeStmt(leftChild.getParaType(), pkb), getTypeStmt(rightChild.getParaType(), pkb));
+		}
+	}
+	return result;
+}
+
+ResultTable Next::evaluate(PKB* pkb, ResultTable* resultTable) {
+	unordered_set<int> left = resultTable->getSynValue(leftChild);
+	unordered_set<int> right = resultTable->getSynValue(rightChild);
+	if (resultTable->getSynCount() == 0) {
+		return evaluate(pkb);
+	}
+	else if (resultTable->getSynCount() == 1) {
+		if (left.size() != 0) {
+			return getNextSynNum(pkb, left, stoi(rightChild.getParaName()));
+		}
+		else if (right.size() != 0) {
+			return getNextNumSyn(pkb, right, stoi(leftChild.getParaName()));
+		}
+	}
+	else if (resultTable->getSynCount() == 2) {
+		if (left.size() == 0) {
+			return getNextSynSyn(pkb, getTypeStmt(leftChild.getParaType(), pkb), right);
+		}
+		else if (right.size() == 0) {
+			return getNextSynSyn(pkb, left, getTypeStmt(rightChild.getParaType(), pkb));
+		}
+		else {
+			return getNextSynSyn(pkb, resultTable);
+		}
+	}
+	return result;
+}
+
+ResultTable Next::getNextSynNum(PKB* pkb, unordered_set<int> left, int right) {
+	result.setSynList(vector<Parameter>({ leftChild }));
+	if (isValidStmtNo(right, pkb)) {
+		unordered_set<int> prev = pkb->getPreviousStmt(right);
+		for (auto& it : prev) {
+			if (left.find(it) != left.end()) {
+				result.insertTuple(vector<int>(it));
+
+			}
+		}
+	}
+	return result;
+}
+
+ResultTable Next::getNextNumSyn(PKB* pkb, unordered_set<int> right, int left) {
+	result.setSynList(vector<Parameter>({ rightChild }));
+	if (isValidStmtNo(left, pkb)) {
+		unordered_set<int> next = pkb->getNextStmt(left);
+		for (auto& it : next) {
+			if (right.find(it) != right.end()) {
+				result.insertTuple(vector<int>(it));
+
+			}
+		}
+	}
+	return result;
+}
+
+ResultTable Next::getNextSynSyn(PKB* pkb, unordered_set<int> left, unordered_set<int> right) {
+	result.setSynList(vector<Parameter>({ leftChild, rightChild }));
+	if (left.size() < right.size()) {
+		for (auto& leftIterator : left) {
+			unordered_set<int> next = pkb->getNextStmt(leftIterator);
+			for (auto& it : next) {
+				if (right.find(it) != right.end()) {
+					result.insertTuple(vector<int>(leftIterator, it));
+
+				}
+			}
+		}
+	}
+	else {
+		for (auto& rightIterator : right) {
+			unordered_set<int> prev = pkb->getPreviousStmt(rightIterator);
+			for (auto& it : prev) {
+				if (left.find(it) != left.end()) {
+					result.insertTuple(vector<int>(it, rightIterator));
+
+				}
+			}
+		}
+	}
+	return result;
+}
+
+ResultTable Next::getNextSynSyn(PKB* pkb, ResultTable* resultTable) {
+	result.setSynList(vector<Parameter>({ leftChild, rightChild }));
+	vector<Parameter> synonyms = resultTable->getSynList();
+	vector<vector<int>> tupleList = resultTable->getTupleList();
+	if (isLeftChild(synonyms[0])) {
+		for (int i = 0; i < tupleList.size(); i++) {
+			if (isNext(pkb, tupleList[i][0], tupleList[i][1])) {
+				result.insertTuple(vector<int>(tupleList[i][0], tupleList[i][1]));
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < tupleList.size(); i++) {
+			if (isNext(pkb, tupleList[i][1], tupleList[i][0])) {
+				result.insertTuple(vector<int>(tupleList[i][1], tupleList[i][0]));
+			}
+		}
+	}
+	return result;
+}
+
+ResultTable Next::getNextNumNum(PKB* pkb, int left, int right) {
+	if (!isValidStmtNo(left, pkb)) {
+		result.setBoolean(false);
+		return result;
+	}
+	if (!isValidStmtNo(right, pkb)) {
+		result.setBoolean(false);
+		return result;
+	}
+	result.setBoolean(isNext(pkb, left, right));
+	return result;
+}
+
+bool Next::isNext(PKB* pkb, int left, int right) {
+	unordered_set<int> prev = pkb->getPreviousStmt(right);
+	if (prev.find(left) != prev.end()) {
+		return true;
+	}
+	return false;
+}
+
+unordered_set<int> Next::getTypeStmt(Type type, PKB* pkb) {
+	switch (type) {
+	case PROG_LINE:
+	case STMT:
+	case ANYTHING: {
+		int numOfStmt = pkb->getNumOfStmt();
+		unordered_set<int> stmtList(numOfStmt);
+		for (int i = 0; i < numOfStmt; i++) {
+			stmtList[i] = i + 1;
+		}
+		return stmtList;
+	}
+	case WHILE:
+		return pkb->getAllWhileStmt();
+	case ASSIGN:
+		return pkb->getAllAssignStmt();
+	case IF:
+		return pkb->getAllIfId();
+	case CALL:
+		return pkb->getAllCallId();
+	}
+	return unordered_set<int>();
+}
+
+bool Next::isNumber(Parameter parameter) {
+	Type type = parameter.getParaType();
+	return (type == INTEGER);
+}
+
+bool Next::isSynonym(Parameter parameter) {
+	Type type = parameter.getParaType();
+	return (type == ASSIGN || type == WHILE || type == STMT || type == ANYTHING || type == PROG_LINE || type == IF || type == CALL);
+}
+
+bool Next::isValidStmtNo(int stmtId, PKB* pkb) {
+	return ((stmtId > 0) && (stmtId <= pkb->getNumOfStmt()));
+}
+
+void Next::insertSynList(Parameter p) {
+	synList.push_back(p);
+}
+
+Parameter Next::getLeftChild() {
+	return leftChild;
+}
+Parameter Next::getRightChild() {
+	return rightChild;
+}
+vector<Parameter> Next::getSynList() {
+	return synList;
+}
+
+/*
+
 ResultTable Next::execute(PKB* pkb) {
 	if (isNumber(leftChild)) {
 		if (isNumber(rightChild)) {
@@ -29,21 +229,6 @@ ResultTable Next::execute(PKB* pkb) {
 	}
 	return result;
 }
-/*
-ResultTable Follow::execute(PKB* pkb, vector<Parameter> parameter, vector<vector<int>> statements) {
-	if (parameter.size() == 1) {
-		if (isLeftChild(parameter[0])) {
-			return getFollowSynNum(pkb, statements);
-		}
-		else if (isRightChild(parameter[0])) {
-			return getFollowNumSyn(pkb, statements);
-		}
-	}
-	else if (parameter.size() == 2) {
-		return getFollowSynSyn(pkb, statements);
-	}
-	return result;
-}*/
 
 ResultTable Next::getNextNumNum(PKB* pkb) {
 	int leftArgument = stoi(leftChild.getParaName());
@@ -81,21 +266,6 @@ ResultTable Next::getNextNumSyn(PKB* pkb) {
 	}
 	return result;
 }
-/*
-ResultTable Follow::getFollowNumSyn(PKB* pkb, vector<vector<int>> statements) {
-	result.setSynList(vector<Parameter>({ rightChild }));
-	int leftArgument = stoi(leftChild.getParaName());
-	if (isValidStmtNo(leftArgument, pkb)) { //if left is a valid statement number, follows(num, syn)
-		int followedBy = pkb->getStmtFollowStmt(leftArgument);
-		if (find(statements[0].begin(), statements[0].end(), followedBy) != statements[0].end()) {
-			if (isStmtType(followedBy, rightChild, pkb)) {
-				result.insertTuple(vector<int>(followedBy));
-				return result;
-			}
-		}
-	}
-	return result;
-}*/
 
 ResultTable Next::getNextSynNum(PKB* pkb) {
 	result.setSynList(vector<Parameter>({ leftChild }));
@@ -111,21 +281,6 @@ ResultTable Next::getNextSynNum(PKB* pkb) {
 	}
 	return result;
 }
-/*
-ResultTable Follow::getFollowSynNum(PKB* pkb, vector<vector<int>> statements) {
-	int rightArgument = stoi(rightChild.getParaName());
-	if (isValidStmtNo(rightArgument, pkb)) { //if right is a valid statement number, follows(syn, num)
-		result.setSynList(vector<Parameter>({ leftChild }));
-		int follows = pkb->getStmtFollowedByStmt(rightArgument);
-		if (find(statements[0].begin(), statements[0].end(), follows) != statements[0].end()) {
-			if (isStmtType(follows, rightChild, pkb)) {
-				result.insertTuple(vector<int>(follows));
-				return result;
-			}
-		}
-	}
-	return result;
-}*/
 
 ResultTable Next::getNextSynSyn(PKB* pkb) {
 	result.setSynList(vector<Parameter>({ leftChild, rightChild }));
@@ -146,50 +301,6 @@ ResultTable Next::getNextSynSyn(PKB* pkb) {
 	}
 	return result;
 }
-/*
-ResultTable Follow::getFollowSynSyn(PKB* pkb, vector<vector<int>> statements) {
-	result.setSynList(vector<Parameter>({ leftChild, rightChild }));
-	if (leftChild.getParaName().compare(rightChild.getParaName()) == 0) {
-		return result;
-	}
-	if (statements.size() == 0) {
-		return result;
-	}
-	if (statements[0].size() == 0) {
-		vector<int> right = statements[1];
-		for (int i = 0; i < right.size(); i++) {
-			int follows = pkb->getStmtFollowedByStmt(right[i]);
-			if (isStmtType(follows, leftChild, pkb)) {
-				result.insertTuple(vector<int>(follows, right[i]));
-			}
-		}
-		return result;
-	}
-	else if (statements[1].size() == 0) {
-		vector<int> left = statements[0];
-		for (int i = 0; i < left.size(); i++) {
-			int follows = pkb->getStmtFollowStmt(left[i]);
-			if (isStmtType(follows, rightChild, pkb)) {
-				result.insertTuple(vector<int>(left[i], follows));
-			}
-		}
-		return result;
-	}
-	else {
-		unordered_set<int> left(statements[0].begin(), statements[0].end());
-		vector<int> right = statements[1];
-		for (int i = 0; i < right.size(); i++) {
-			int follows = pkb->getStmtFollowedByStmt(right[i]);
-			if (left.find(follows) != left.end()) {
-				if (isStmtType(follows, leftChild, pkb)) {
-					result.insertTuple(vector<int>(follows, right[i]));
-				}
-			}
-		}
-		return result;
-	}
-}
-*/
 
 vector<int> Next::getTypeStmt(Type type, PKB* pkb) {
 	switch (type) {
@@ -262,4 +373,4 @@ Parameter Next::getRightChild() {
 }
 vector<Parameter> Next::getSynList() {
 	return synList;
-}
+}*/

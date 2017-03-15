b827287baa95bba674c446f9e76c2b6072f4923b
Guo Xiang <A0112659@u.nus.edu>
2017-03-15 20:15:53 +0800

    Update PQL.cpp

--- SPA_Project/source/PQL.cpp
+++ SPA_Project/source/PQL.cpp
@@ -6,6 +6,7 @@
 
 PQL::PQL(PKB *pkbSource) {
 	pkb = pkbSource;
+	preProcessor = new QueryParser();
 	evaluator = new QueryEvaluator(pkb);
 	postProcessor = new QueryPostProcessor(pkb);
 }
@@ -16,16 +17,26 @@ PQL::~PQL() {
 }
 
 list<string> PQL::evaluateQuery(string querySource) {
+	string test = "procedure p1; assign a2; variable v3; stmt s; while w; if ifs; Select a2 such that Next*(a2, w) and Modifies(a2, v3)";
 	QueryTree queryTree;
 	/*
 	if (preProcessor.isValid(querySource)) {
-		queryTree = preProcessor.getQuery();
+	queryTree = preProcessor.getQuery();
 	}
 	else {
-		cout << "invalid query";
-		return list<string>();
+	cout << "invalid query";
+	return list<string>();
 	}
 	*/
+	try {
+		queryTree = preProcessor->parse(querySource);
+	}
+	catch (string msg) {
+		cerr << "\n error is: " + msg << endl;
+		return list<string>();
+	}
+
 	ResultTable result = evaluator->evaluate(queryTree);
-	return postProcessor->processResult(result);
+	list<string> resultList = postProcessor->processResult(result);
+	return list<string>();
 }

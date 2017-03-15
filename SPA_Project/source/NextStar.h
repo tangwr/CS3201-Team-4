c346ca799678188de9b040be42baf0b652a154a2
bthjehiel <jehielmisc@gmail.com>
2017-03-15 23:47:01 +0800

    Update NextStar.h

--- SPA_Project/source/NextStar.h
+++ SPA_Project/source/NextStar.h
@@ -16,14 +16,36 @@ private:
 	Parameter rightChild;
 	vector<Parameter> synList;
 	ResultTable result;
+	bool isNumber(Parameter);
+	bool isSynonym(Parameter);
+	bool isValidStmtNo(int, PKB*);
+	bool isStmtType(int, Parameter, PKB*);
+	bool isNextStar(PKB*, int, int);
+	bool isNextStar(PKB*, int, int, unordered_set<int>*);
+	unordered_set<int> getTypeStmt(Type, PKB*);
+	bool isLeftChild(Parameter);
+	bool isRightChild(Parameter);
+	ResultTable getNextStarNumNum(PKB*, int, int);
+	ResultTable getNextStarNumSyn(PKB*, unordered_set<int>, int);
+	ResultTable getNextStarSynNum(PKB*, unordered_set<int>, int);
+	ResultTable getNextStarSynSyn(PKB*, unordered_set<int>, unordered_set<int>);
+	ResultTable getNextStarSynSyn(PKB*, ResultTable*);
+	ResultTable evaluateWithoutRestrictions(PKB*);
+	ResultTable evaluateWithoutOneRestriction(PKB*, ResultTable*);
+
+	void getAllNextStar(int, unordered_set<int>*, PKB*);
+	void getAllNextStar(int, unordered_set<int>*, unordered_set<int>*, PKB*);
+	void getAllPrevStar(int, unordered_set<int>*, PKB*);
+	void getAllPrevStar(int, unordered_set<int>*, unordered_set<int>*, PKB*);
 
 public:
 	NextStar(Parameter lc, Parameter rc);
 	Parameter getLeftChild();
 	Parameter getRightChild();
-	ResultTable evaluate(PKB*, ResultTable);
+	ResultTable evaluate(PKB*, ResultTable*);
 	void insertSynList(Parameter);
 	vector<Parameter> getSynList();
 	ClauseType getClauseType();
+	ResultTable evaluate(PKB* pkb, ResultTable intResultTable);
 };
 #endif

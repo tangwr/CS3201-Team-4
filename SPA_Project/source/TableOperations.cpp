ab6f1386c698e8d7368d4a39f236419298788a1e
swxsw <suwei9@gmail.com>
2017-03-18 13:57:55 +0800

    Added util method to check if 2 unordered sets are equal
    Added util method to check if 2 unordered sets are equal
    Have equal contents (not same object)

--- SPA_Project/source/TableOperations.cpp
+++ SPA_Project/source/TableOperations.cpp
@@ -11,3 +11,17 @@ void TableOperations::printTableVector(vector<int> vList) {
         cout << element << ' ';
     }
 }
+bool TableOperations::isEqualUnorderedSet(unordered_set<int> uSet1, unordered_set<int> uSet2) {
+    //check size
+    if (((int)uSet1.size) != ((int)uSet2.size)) {
+        return false;
+    }
+    // check one contains the other
+    for (int element : uSet1) {
+        if (uSet2.find(element) == uSet2.end()) {
+            return false;
+        }
+    }
+    
+    return true;
+}

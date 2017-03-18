4056025d8a713c5a06989d14c922ebb0a952ac97
bthjehiel <jehielmisc@gmail.com>
2017-03-18 16:42:30 +0800

    Unit test for next

---
+++ SPA_Project/UnitTesting/PKBStubNextStar.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "PKB.h"
+
+class PKBStubNextStar : public PKB {
+public:
+	PKBStubNextStar();
+
+	unordered_set<int> getPreviousStmt(int);
+	unordered_set<int> getNextStmt(int);
+	int getNumOfStmt();
+	unordered_set<int> getAllWhileStmt();
+	unordered_set<int> getAllAssignStmt();
+	unordered_set<int> getAllIfId();
+	unordered_set<int> getAllCallId();
+
+
+
+private:
+	unordered_set<int> assignStmts;
+	unordered_set<int> whileStmts;
+	unordered_set<int> ifStmts;
+	unordered_set<int> callStmts;
+	unordered_map<int, unordered_set<int>> nextTable;
+	unordered_map<int, unordered_set<int>> prevTable;
+};

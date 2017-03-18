d38818636536f43d403c221eba3d6ad6ca879859
bthjehiel <jehielmisc@gmail.com>
2017-03-18 18:02:53 +0800

    Update NextStar.cpp

--- SPA_Project/source/NextStar.cpp
+++ SPA_Project/source/NextStar.cpp
@@ -8,6 +8,12 @@ using namespace std;
 NextStar::NextStar(Parameter lc, Parameter rc) {
 	leftChild = lc;
 	rightChild = rc;
+	if (leftChild.isSynonym()) {
+		synList.push_back(leftChild);
+	}
+	if (rightChild.isSynonym()) {
+		synList.push_back(rightChild);
+	}
 }
 
 ResultTable NextStar::evaluateWithoutRestrictions(PKB* pkb) {
@@ -74,7 +80,9 @@ ResultTable NextStar::getNextStarSynNum(PKB* pkb, unordered_set<int> left, int r
 		unordered_set<int> allPrevStar;
 		getAllPrevStar(right, &allPrevStar, pkb);
 		for (auto& it : allPrevStar) {
-			result.insertTuple(vector<int>(1,it));
+			if (left.find(it) != left.end()) {
+				result.insertTuple(vector<int>(1, it));
+			}
 		}
 	}
 	return result;
@@ -87,7 +95,9 @@ ResultTable NextStar::getNextStarNumSyn(PKB* pkb, unordered_set<int> right, int 
 		unordered_set<int> allNextStar;
 		getAllNextStar(leftArgument, &allNextStar, pkb);
 		for (auto& it : allNextStar) {
-			result.insertTuple(vector<int>(1, it));
+			if (right.find(it) != right.end()) {
+				result.insertTuple(vector<int>(1, it));
+			}
 		}
 	}
 	return result;
@@ -103,8 +113,10 @@ ResultTable NextStar::getNextStarSynSyn(PKB* pkb, unordered_set<int> left, unord
 			unordered_set<int> allNextStar;
 			getAllNextStar(leftIterator, &allNextStar, pkb);
 			for (auto& it : allNextStar) {
-				vector<int> tuple = { leftIterator, it };
-				result.insertTuple(tuple);
+				if (right.find(it) != right.end()) {
+					vector<int> tuple = { leftIterator, it };
+					result.insertTuple(tuple);
+				}
 			}
 		}
 	}
@@ -113,8 +125,10 @@ ResultTable NextStar::getNextStarSynSyn(PKB* pkb, unordered_set<int> left, unord
 			unordered_set<int> allPrevStar;
 			getAllPrevStar(rightIterator, &allPrevStar, pkb);
 			for (auto& it : allPrevStar) {
-				vector<int> tuple = { it, rightIterator };
-				result.insertTuple(tuple);
+				if (left.find(it) != left.end()) {
+					vector<int> tuple = { it, rightIterator };
+					result.insertTuple(tuple);
+				}
 			}
 		}
 	}
@@ -217,7 +231,7 @@ unordered_set<int> NextStar::getTypeStmt(Type type, PKB* pkb) {
 	}
 	return unordered_set<int>();
 }
-
+/*
 bool NextStar::isStmtType(int stmtId, Parameter parameter, PKB* pkb) {
 	Type type = parameter.getParaType();
 	if (stmtId < 1)
@@ -233,7 +247,7 @@ bool NextStar::isStmtType(int stmtId, Parameter parameter, PKB* pkb) {
 		return true;
 	}
 	return false;
-}
+}*/
 
 bool NextStar::isNumber(Parameter parameter) {
 	Type type = parameter.getParaType();
@@ -264,10 +278,6 @@ Parameter NextStar::getRightChild() {
 	return rightChild;
 }
 
-void NextStar::insertSynList(Parameter p) {
-	synList.push_back(p);
-}
-
 vector<Parameter> NextStar::getSynList() {
 	return synList;
 }
@@ -289,9 +299,9 @@ void NextStar::getAllNextStar(int prev, unordered_set<int>* allNextStar, unorder
 	visited->insert(prev);
 	unordered_set<int> next = pkb->getNextStmt(prev);
 	for (auto& it : next) {
-		if (isStmtType(it, rightChild, pkb)) {
+		//if (isStmtType(it, rightChild, pkb)) {
 			allNextStar->insert(it);
-		}
+		//}
 		getAllNextStar(it, allNextStar, visited, pkb);
 	}
 	return;
@@ -310,189 +320,10 @@ void NextStar::getAllPrevStar(int next, unordered_set<int>* allPrevStar, unorder
 	visited->insert(next);
 	unordered_set<int> prev = pkb->getPreviousStmt(next);
 	for (auto& it : prev) {
-		if (isStmtType(it, leftChild, pkb)) {
+		//if (isStmtType(it, leftChild, pkb)) {
 			allPrevStar->insert(it);
-		}
+		//}
 		getAllPrevStar(it, allPrevStar, visited, pkb);
 	}
 	return;
 }
-/*
-ResultTable NextStar::execute(PKB* pkb) {
-if (isNumber(leftChild)) {
-if (isNumber(rightChild)) {
-return getNextStarNumNum(pkb);
-}
-else if (isSynonym(rightChild)) {
-return getNextStarNumSyn(pkb);
-}
-}
-else if (isSynonym(leftChild)) {
-if (isNumber(rightChild)) {
-return getNextStarSynNum(pkb);
-}
-else if (isSynonym(rightChild)) {
-return getNextStarSynSyn(pkb);
-}
-}
-return result;
-}
-
-ResultTable NextStar::getNextStarNumNum(PKB* pkb) {
-int leftArgument = stoi(leftChild.getParaName());
-int rightArgument = stoi(rightChild.getParaName());
-if (!isValidStmtNo(leftArgument, pkb)) {
-result.setBoolean(false);
-return result;
-}
-if (!isValidStmtNo(rightArgument, pkb)) {
-result.setBoolean(false);
-return result;
-}
-
-unordered_set<int> prev = pkb->getPreviousStmt(rightArgument);
-if (prev.find(leftArgument) != prev.end()) {
-result.setBoolean(true);
-return result;
-}
-result.setBoolean(false);
-return result;
-}
-
-ResultTable NextStar::getNextStarNumSyn(PKB* pkb) {
-result.setSynList(vector<Parameter>({ rightChild }));
-int leftArgument = stoi(leftChild.getParaName());
-if (isValidStmtNo(leftArgument, pkb)) { //if left is a valid statement number, Next(num, syn)
-unordered_set<int> allNextStar;
-getAllNextStar(leftArgument, allNextStar, pkb);
-}
-return result;
-}
-
-ResultTable NextStar::getNextStarSynNum(PKB* pkb) {
-result.setSynList(vector<Parameter>({ leftChild }));
-int rightArgument = stoi(rightChild.getParaName());
-if (isValidStmtNo(rightArgument, pkb)) { //if right is a valid statement number, Next(syn, num)
-unordered_set<int> allPrevStar;
-getAllPrevStar(rightArgument, allPrevStar, pkb);
-}
-return result;
-}
-
-ResultTable NextStar::getNextStarSynSyn(PKB* pkb) {
-result.setSynList(vector<Parameter>({ leftChild, rightChild }));
-
-vector<int> left = getTypeStmt(leftChild.getParaType(), pkb);
-unordered_set<int> allNextStar;
-for (int i = 0; i < left.size(); i++) {
-getAllPrevStar(left[i], allNextStar, pkb);
-}
-/*
-vector<int> right = getTypeStmt(rightChild.getParaType(), pkb);
-return getAllPrevStar(right, pkb);*//*
-return result;
-}
-
-vector<int> NextStar::getTypeStmt(Type type, PKB* pkb) {
-switch (type) {
-case PROG_LINE:
-case STMT:
-case ANYTHING: {
-int numOfStmt = pkb->getNumOfStmt();
-vector<int> stmtList(numOfStmt);
-for (int i = 0; i < numOfStmt; i++) {
-stmtList[i] = i + 1;
-}
-return stmtList;
-}
-case WHILE:
-//return pkb->getAllWhileStmt();
-case ASSIGN:;
-//return pkb->getAllAssignStmt();
-}
-return vector<int>();
-}
-
-
-void NextStar::getAllNextStar(int prev, unordered_set<int> allNextStar, PKB* pkb) {
-if (allNextStar.find(prev) != allNextStar.end()) {
-return;
-}
-allNextStar.insert(prev);
-unordered_set<int> next = pkb->getNextStmt(prev);
-for (auto& it : next) {
-if (isStmtType(it, rightChild, pkb)) {
-result.insertTuple(vector<int>(prev, it));
-}
-getAllNextStar(it, allNextStar, pkb);
-}
-return ;
-}
-
-void NextStar::getAllPrevStar(int next, unordered_set<int> allPrevStar, PKB* pkb) {
-if (allPrevStar.find(next) != allPrevStar.end()) {
-return;
-}
-allPrevStar.insert(next);
-unordered_set<int> prev = pkb->getPreviousStmt(next);
-for (auto& it : prev) {
-if (isStmtType(it, leftChild, pkb)) {
-result.insertTuple(vector<int>(it, next));
-}
-getAllPrevStar(it, allPrevStar, pkb);
-}
-return;
-}
-
-bool NextStar::isStmtType(int stmtId, Parameter parameter, PKB* pkb) {
-Type type = parameter.getParaType();
-if (stmtId < 1)
-return false;
-switch (type) {
-case WHILE:
-return pkb->isStmtInWhileTable(stmtId);
-case ASSIGN:
-return pkb->isStmtInAssignTable(stmtId);
-case PROG_LINE:
-case STMT:
-case ANYTHING:
-return true;
-}
-return false;
-}
-
-void NextStar::insertSynList(Parameter p) {
-synList.push_back(p);
-}
-
-bool NextStar::isNumber(Parameter parameter) {
-Type type = parameter.getParaType();
-return (type == INTEGER);
-}
-
-bool NextStar::isSynonym(Parameter parameter) {
-Type type = parameter.getParaType();
-return (type == ASSIGN || type == WHILE || type == STMT || type == ANYTHING || type == PROG_LINE);
-}
-
-bool NextStar::isValidStmtNo(int stmtId, PKB* pkb) {
-return ((stmtId > 0) && (stmtId <= pkb->getNumOfStmt()));
-}
-
-Parameter NextStar::getLeftChild() {
-return leftChild;
-}
-Parameter NextStar::getRightChild() {
-return rightChild;
-}
-vector<Parameter> NextStar::getSynList() {
-return synList;
-}
-
-ClauseType NextStar::getClauseType() {
-return NEXTSTAR;
-}
-
-ResultTable NextStar::evaluate(PKB* pkb, ResultTable intResultTable) {
-return ResultTable();
-}*/

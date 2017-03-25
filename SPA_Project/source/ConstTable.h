e89719b2ceec4d144dc561b510bfdd620df50e50
swxsw <suwei9@gmail.com>
2017-03-25 15:28:55 +0800

    Updated const and var table
    Now using set to store, so get all now is O1 instead of On

--- SPA_Project/source/ConstTable.h
+++ SPA_Project/source/ConstTable.h
@@ -5,26 +5,34 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>;
 
 using namespace std;
 
 class ConstTable {
 private:
 
-	vector<int> constList;   // constId -> constName = Value
-	unordered_map<int, int> constMap;    // constName -> constId
+	//vector<int> constList;   // constId -> constName = Value
+    unordered_set<int> constSet;
+	//unordered_map<int, int> constMap;    // constName -> constId
 
 	int ctsize; // number of variables in the program
 
 public:
 	ConstTable(void);
-	int insertConst( int value);
+	//int insertConst( int value);
 	int getSize();
-	int getConstName(int constId);
-	int getConstIndex(int constName);
-	bool checkConstExist(int constName);
-	int getValueById(int constId);
-	void printContents();
-	vector<int> getAllConst();
-    vector<int> getAllConstId();
+	//int getConstName(int constId);
+	//int getConstIndex(int constName);
+	//bool checkConstExist(int constName);
+	//int getValueById(int constId);
+	//void printContents();
+	//vector<int> getAllConst();
+    //vector<int> getAllConstId();
+
+    //updated methods
+    bool insertConst(int value);
+    bool checkConstExist(int constValue);
+    void printContents();
+    unordered_set<int> getAllConst();
 };
--- SPA_Project/source/VarTable.h
+++ SPA_Project/source/VarTable.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ class VarTable {
 private:
 	vector<string> varList;   // varId -> varName
 	unordered_map<string, int> varMap;    // varName -> varId
+    unordered_set<int> varIdSet;
 	int vtsize; // number of variables in the program
 
 public:
@@ -20,8 +22,8 @@ public:
 	int getSize();
 	string getVarNameById(int varId);
 	int getVarIndex(string varName);
+    unordered_set<int> getAllVarId();
 	bool checkVarExistByName(string varName);
 	bool checkVarExistById(int varId);
-    vector<int> getAllVarId();
     void printContents();
 };

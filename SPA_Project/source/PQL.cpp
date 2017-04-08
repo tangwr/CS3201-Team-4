22cb80520be7126345c544d69f8df79b06378de4
Guo Xiang <A0112659@u.nus.edu>
2017-04-08 19:11:35 +0800

    Update PQL.cpp

--- SPA_Project/source/PQL.cpp
+++ SPA_Project/source/PQL.cpp
@@ -20,12 +20,13 @@ PQL::~PQL() {
 list<string> PQL::evaluateQuery(string querySource) {
 	string test = "procedure p1; assign a2; variable v3; stmt s; while w; if ifs; Select a2 such that Next*(a2, w) and Modifies(a2, v3)";
 	string test2 = "stmt s; Select s such that Follows(s, a)";
+	string test3 = "if ifs; Select ifs such that Follows(5, ifs)";
 	const int EQUAL = 0;
 	const string FALSE = "FALSE";
 	QueryTree queryTree;
 
 	try {
-		queryTree = preProcessor->parse(test2);
+		queryTree = preProcessor->parse(querySource);
 	}
 	catch (const logic_error& e) {
 		//cerr <<e.what();

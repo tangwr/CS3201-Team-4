b89d4c9e557dc778f4bece84df4564f7f069f624
Guo Xiang <A0112659@u.nus.edu>
2017-04-08 16:08:31 +0800

    throw different exceptions

--- SPA_Project/source/PQL.cpp
+++ SPA_Project/source/PQL.cpp
@@ -19,32 +19,23 @@ PQL::~PQL() {
 
 list<string> PQL::evaluateQuery(string querySource) {
 	string test = "procedure p1; assign a2; variable v3; stmt s; while w; if ifs; Select a2 such that Next*(a2, w) and Modifies(a2, v3)";
-	const string ERROR_SELECT_BOOLEAN = "The result is false";
+	string test2 = "stmt s; Select BOOLEAN such that Follows(s, a)";
 	const int EQUAL = 0;
 	const string FALSE = "FALSE";
 	QueryTree queryTree;
-	/*
-	if (preProcessor.isValid(querySource)) {
-	queryTree = preProcessor.getQuery();
-	}
-	else {
-	cout << "invalid query";
-	return list<string>();
-	}
-	*/
+
 	try {
 		queryTree = preProcessor->parse(querySource);
 	}
+	catch (const logic_error& e) {
+		//cerr <<e.what();
+		list<string> boolResult;
+		boolResult.push_back(FALSE);
+		return boolResult;
+	}
 	catch (string msg) {
-		cerr << "\n error is: " + msg << endl;
-		if (msg.compare(ERROR_SELECT_BOOLEAN) == EQUAL) {
-			list<string> boolResult;
-			boolResult.push_back(FALSE);
-			return boolResult;
-		}
-		else {
-			return list<string>();
-		}
+		//cerr << "\n The error is: " + msg;
+	    return list<string>();
 	}
 
 	ResultTable result = evaluator->evaluate(queryTree);

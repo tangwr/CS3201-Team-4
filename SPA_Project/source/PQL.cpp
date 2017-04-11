8883a1ec1a360cef37a860cb31106d06fbb6e553
Guo Xiang <A0112659@u.nus.edu>
2017-04-11 21:37:25 +0800

    Updated PQL.cpp

--- SPA_Project/source/PQL.cpp
+++ SPA_Project/source/PQL.cpp
@@ -1,8 +1,9 @@
+#pragma once
 #include<stdio.h>
 #include <iostream>
 #include <string>
-
 #include "PQL.h"
+#define FALSE_OUTPUT "FALSE"
 
 PQL::PQL(PKB *pkbSource) {
 	pkb = pkbSource;
@@ -13,30 +14,23 @@ PQL::PQL(PKB *pkbSource) {
 
 PQL::~PQL() {
 	delete evaluator;
-	delete postProcessor;
 	delete preProcessor;
+	delete postProcessor;
 }
 
 list<string> PQL::evaluateQuery(string querySource) {
-	string test = "procedure p1; assign a2; variable v3; stmt s; while w; if ifs; Select a2 such that Next*(a2, w) and Modifies(a2, v3)";
-	string test2 = "stmt s; Select s such that Follows(s, a)";
-	string test3 = "if ifs; Select ifs such that Follows(5, ifs)";
-	const int EQUAL = 0;
-	const string FALSE = "FALSE";
 	QueryTree queryTree;
 
 	try {
 		queryTree = preProcessor->parse(querySource);
 	}
 	catch (const logic_error& e) {
-		//cerr <<e.what();
 		list<string> boolResult;
-		boolResult.push_back(FALSE);
+		boolResult.push_back(FALSE_OUTPUT);
 		return boolResult;
 	}
-	catch (const runtime_error& ie) {
-		//cerr << ie.what();
-	    	return list<string>();
+	catch (const runtime_error& e) {
+	    return list<string>();
 	}
 
 	ResultTable result = evaluator->evaluate(queryTree);

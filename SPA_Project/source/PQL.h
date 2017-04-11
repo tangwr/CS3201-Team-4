4134f4c3781fccb32de67293e8de6933ce63bcce
Guo Xiang <A0112659@u.nus.edu>
2017-04-11 21:36:38 +0800

    Updated PQL.h

--- SPA_Project/source/PQL.h
+++ SPA_Project/source/PQL.h
@@ -14,14 +14,22 @@ using namespace std;
 #include "ResultTable.h"
 
 class PQL {
+public:
+	PQL(PKB *pkb);
+	~PQL();
+	/**
+	* Take in the query and evaluate it
+	*
+	* @param query string from queries code
+	* @return a list of string which contains the result
+	* @throws if the query is invalid, the corresponding exception
+	* will be thrown
+	*/
+	list<string> evaluateQuery(string querySource);
+
 private:
 	PKB *pkb;
 	QueryParser *preProcessor;
 	QueryEvaluator *evaluator;
 	QueryPostProcessor *postProcessor;
-
-public:
-	PQL(PKB *pkb);
-	~PQL();
-	list<string> evaluateQuery(string querySource);
 };
